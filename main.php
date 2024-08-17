fb0cd189c13b0d7ca91a52763beb9bf95fef722a
yananob <yananob@gmail.com>
2024-08-17 21:23:04 +0900

    miro 1st api fixed

--- main.php
+++ main.php
@@ -10,18 +10,21 @@ function main()
 {
     $logger = new Logger();
 
-    $config = file_get_contents("./configs/config.json");
+    $config = json_decode(file_get_contents("./configs/config.json"), false);
+    if ($config === false) {
+        throw new Exception("Failed to load config file");
+    }
 
-    $miroBoard = new MiroBoard($config["miro"]["secret"], $config["miro"]["board_id"]);
-    $gpt = new Gpt($config["gpt"]["secret"], $config["gpt"]["model"]);
+    $miroBoard = new MiroBoard($config->miro->token, $config->miro->board_id);
+    $gpt = new Gpt($config->gpt->secret, $config->gpt->model);
 
     while (true) {
         $miroItems = $miroBoard->readRecentItems(5);
 
         foreach ($miroItems as $miroItem) {
-            $logger->info("processing miroItem: {$miroItem->text}");
+            $logger->info("processing miroItem: {$miroItem->data->content}");
 
-            $comment = $gpt->getComment($miroItem->text);
+            $comment = $gpt->getComment($miroItem->data->content);
             $logger->info("comment from gpt: {$comment}");
 
             $miroBoard->putComment($miroItem, $comment);
